d77e4265a3be1c3a9dd0d8ad02580cdce9ade2a6
Quynh Thi Ho <qthi.ho@gmail.com>
2020-04-12 20:48:03 -0700

    first run for CO after updates

--- scripts/burned_in_and_tune.R
+++ scripts/burned_in_and_tune.R
@@ -8,7 +8,6 @@ aco_burned <- burn_n_thin_draws(aco_draws, jump = 1, burn_at = 1e3)
 
 aco_burned %>% 
   rowid_to_column('iteration') %>% 
-  # mutate(iteration = ) %>% 
   reshape2::melt(id.vars = "iteration") %>%
   ggplot(aes(x = iteration, y = value, color = variable)) +
   geom_line(size = 0.1) +
@@ -25,8 +24,34 @@ cov(aco_burned[,-c(1:2)])
 apply(aco_burned, 2, median)
 apply(aco_burned, 2, mean)
 
+coda::effectiveSize(aco_burned[,1:2])
+coda::effectiveSize(aco_burned[,1])
 
 
+# ggplot for autocorrelation graphs ---------------------------------------
+alpha_theta_draws <- co_burned %>% 
+  as_tibble() %>% 
+  select(alpha, theta) #%>% 
+  # burn_n_thin_draws(jump = 260, burn_at = 1e3)
+
+coda::effectiveSize(outcomes[,1:2])
+coda::effectiveSize(outcomes[,1])
+
+alpha <- 0.95
+alpha.post.acf <- acf(alpha_theta_draws, plot=FALSE, lag.max = 1000)
+alpha.acf_df <- data.frame(corr = alpha.post.acf$acf) %>% 
+  mutate(lags = 1:n())
+conf.lims <- c(-1,1) * qnorm((1 + alpha)/2)/sqrt(alpha.post.acf$n.used)
+
+alpha.acf_df %>% 
+  ggplot(aes(x=lags, y = corr.1)) + 
+  labs(y="Autocorrelations", x="Lag", title= "Autocorrelation for alpha") +
+  geom_segment(aes(xend=lags, yend=0)) +
+  # geom_point() +
+  # ylim(c(-0.2,0.2)) +
+  # xlim(c(800, 1000)) +
+  geom_hline(yintercept=conf.lims, lty=2, col='blue') +
+  theme_minimal()
 
 
 
@@ -38,8 +63,8 @@ apply(aco_burned, 2, mean)
 
 #### CO -------------------------------- ####
 
-co_draws <- read.csv('data/posterior_co_trial_2020-04-12.csv')
-co_burned <- burn_n_thin_draws(co_draws, jump = 1, burn_at = 1e5)
+co_draws <- read.csv('data/posterior_co_trials_recent_run.csv')
+co_burned <- burn_n_thin_draws(co_draws, jump = 260, burn_at = 1e3)
 
 
 co_burned %>% 
@@ -48,7 +73,7 @@ co_burned %>%
   reshape2::melt(id.vars = "iteration") %>%
   ggplot(aes(x = iteration, y = value, color = variable)) +
   geom_line(size = 0.1) +
-  # geom_hline(yintercept = 0, color = "brown3", size = 0.5, linetype = "dashed") +
+  geom_hline(yintercept = 0, color = "brown3", size = 0.5, linetype = "dashed") +
   facet_wrap(~variable, ncol = 3, scales = "free_y") +
   labs(y = "coefficients") +
   theme_minimal() +
--- scripts/posterior dist generator.R
+++ scripts/posterior dist generator.R
@@ -20,7 +20,7 @@ is_dummy <- is_data %>%
 
 #### step 2: initial values and draws from MH algorithm ####
 
-B <- 300000
+B <- 3000
 a1 <- a2 <- 0
 
 outcomes <- matrix(0, nrow = B + 1, ncol = ncol(is_dummy)-1+2)
@@ -148,7 +148,7 @@ is_dummy <- is_data %>% filter(Population == "CO") %>% dummy_co()
 
 #### step 2: initial values and draws from MH algorithm ####
 
-B <- 300000
+B <- 500000
 a1 <- a2 <- 0
 
 outcomes <- matrix(0, nrow = B + 1, ncol = ncol(is_dummy)-1+2)
@@ -158,23 +158,27 @@ colnames(outcomes) <- c("alpha", "theta", colnames(is_dummy)[-1])
 #   0.735576928,   0.250055192,  -0.015691754,  -0.884285239,  -0.906604992,   -0.521759576,  -0.309321503,
 #   0.158424317,   0.096360510,   0.325413932,   0.152977188,   0.064844156,   0.006385447,  -0.182813001,   
 #   0.042295215 )
-
-outcomes[1,] <- c(
-  3.95744600,    0.31253430,    2.67353700,   -0.88902345,    0.18291240,    
-  1.33515900,    1.04234250,    0.81375680,    0.45575790,    0.11387325 ,   
-  0.59973100,    0.61522040,   -0.31968705,   0.31373570,   -0.10320105,   
-  -0.07326436,   -0.15598775,   -0.09338692,   -0.13881100,   -0.79989680,   -0.10959390,    0.02975302
-)
+# 2020_04_12 changed
+# outcomes[1,] <- c(
+#   3.95744600,    0.31253430,    2.67353700,   -0.88902345,    0.18291240,    
+#   1.33515900,    1.04234250,    0.81375680,    0.45575790,    0.11387325 ,   
+#   0.59973100,    0.61522040,   -0.31968705,   0.31373570,   -0.10320105,   
+#   -0.07326436,   -0.15598775,   -0.09338692,   -0.13881100,   -0.79989680,   -0.10959390,    0.02975302
+# )
+# inital values started from last draw of recent draws
+outcomes[1, ] <- read_csv('data/posterior_co_trials_recent_run.csv') %>% 
+  last() %>% 
+  unlist()
 
 #### step 3: for loop of MH algorithm
-
+start_time <- Sys.time()
 for(iter in 2:(B+1)){
   
   #### ---------------------- MH for shape ---------------------- ####
   # propose new shape
   log_shape_proposed = log_shape_sampler2(log_alpha = log(outcomes[iter-1,1]),
                                           log_theta = log(outcomes[iter-1,2]),
-                                          multiplier = 0.05)
+                                          multiplier = 0.1)
   loglike = log_likelihood(data = is_dummy,
                            alpha = exp(log_shape_proposed[1]),
                            theta = exp(log_shape_proposed[2]),
@@ -258,7 +262,7 @@ cat(paste0("acceptance rate for coefs:  ", a2/B, "\n"))
 
 # outcomes %>% burn_n_thin_draws(burn_at = 1, jump=1) %>% apply(2, function(c) acf(c))
 outcomes %>%
-  burn_n_thin_draws(burn_at = 1, jump = 1) %>%
+  burn_n_thin_draws(burn_at = 1, jump = 260) %>%
   as.data.frame() %>%
   rownames_to_column("iteration") %>%
   reshape2::melt(id.vars = "iteration") %>%
@@ -269,9 +273,11 @@ outcomes %>%
   theme_minimal() +
   theme(legend.position = "none")
 
+end_time <- Sys.time()
+end_time - start_time
 
 #### step 4: save posterior dist and acceptance rate ####
 
 outcomes = rbind(c(rep(a1/B, 2), rep(a2/B, ncol(outcomes)-2)), outcomes)
-write.csv(outcomes, file = "./data/posterior_co_trial_2020-04-12.csv", row.names = FALSE)
+write.csv(outcomes, file = "./data/posterior_co_trials_next_500k.csv", row.names = FALSE)
 
